644ae8c101f11feac8f6a61f6c4c67802c5aca04
Angelo <angelorpt@gmail.com>
2020-03-27 10:30:46 -0400

    Salvando Produto no Banco

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -9,6 +9,15 @@ namespace ASPNetCore_API.Controllers
     [Route("api/[controller]")]
     public class ProdutosController : ControllerBase
     {
+
+        private readonly ApplicationDbContext _database;
+        
+        // Constructor
+        public ProdutosController(ApplicationDbContext database)
+        {
+            this._database = database;
+        }
+
         [HttpGet]
         public IActionResult Get()
         {
@@ -24,7 +33,15 @@ namespace ASPNetCore_API.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] Produto produto)
         {
-            return Ok(new { msg = "Produto salvo com sucesso", produto = produto });
+            Produto produto_new = new Produto();
+
+            produto_new.Nome  = produto.Nome;
+            produto_new.Preco = produto.Preco;
+
+            _database.Add(produto_new);
+            _database.SaveChanges();
+
+            return Ok(new { msg = "Produto salvo com sucesso", produto = produto_new });
         }
     }
 }
