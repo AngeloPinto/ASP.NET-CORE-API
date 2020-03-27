bdcc7e715ac44d7a79dfc5ee2c4430c3e0aee905
Angelo <angelorpt@gmail.com>
2020-03-27 11:00:21 -0400

    Deletando um recurso de produto

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -2,8 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ASPNetCore_API.Data;
 using ASPNetCore_API.Models;
-using System.Linq;
 using System;
+using System.Linq;
 
 namespace ASPNetCore_API.Controllers
 {
@@ -34,10 +34,12 @@ namespace ASPNetCore_API.Controllers
                 var Produto = _database.Produtos.Find(id);
                 return Ok(Produto);
             } catch(Exception e) {
-                return BadRequest(new {msg = "Id Inválido", error = e});
+                Response.StatusCode = 404;
+                return new ObjectResult(new {msg = "Id Inválido", error = e});
             }
         }
 
+        // POST
         [HttpPost]
         public IActionResult Post([FromBody] Produto produto)
         {
@@ -49,7 +51,36 @@ namespace ASPNetCore_API.Controllers
             _database.Add(produto_new);
             _database.SaveChanges();
 
-            return Ok(new { msg = "Produto salvo com sucesso", produto = produto_new });
+            Response.StatusCode = 201;
+            var result = new ObjectResult(
+                new 
+                { 
+                    msg = "Produto salvo com sucesso",
+                    produto = produto_new 
+                }
+            );
+
+            return result;
+        }
+
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try {
+                
+                var Produto = _database.Produtos.Find(id);
+                _database.Produtos.Remove(Produto);
+                _database.SaveChanges();
+
+                return Ok(new { msg = "Recurso deletado", produto = Produto } );
+
+            } catch (Exception e) {
+                
+                Response.StatusCode = 404;
+                return new ObjectResult(new {msg = "Id Inválido", error = e});
+            }            
         }
     }
 }
