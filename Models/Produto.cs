b423fdd48273ad68daecb3a632d249ccd0ade407
Angelo <angelorpt@gmail.com>
2020-03-27 11:25:52 -0400

    Validando os campos da Classe Produto

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ASPNetCore_API.Models
 {
     public class Produto
     {
         public int Id { get; set; }
+        
+        [Required]
         public string Nome { get; set; }
+        
+        [Required]
+        [Range(minimum: 0.00, maximum: (double) decimal.MaxValue)]
         public float Preco { get; set; }
     }
 }
