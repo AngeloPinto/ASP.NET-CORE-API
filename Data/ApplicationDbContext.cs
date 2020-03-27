de2ff78a31a7a43171d056939aa1bc7ead07dd3d
Angelo <angelorpt@gmail.com>
2020-03-27 09:27:44 -0400

    Criando a Classe produto e referenciando ela no contexto

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
-using Microsoft.EntityFrameworkCore;
 using System;
+using Microsoft.EntityFrameworkCore;
+using ASPNetCore_API.Models;
 
 namespace ASPNetCore_API.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Produto> Produtos { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base() {}
     }
 }
